da386eff4bcb7b6c90b8f490bb41cc584d7011f2
twocucao <twocucao@gmail.com>
2023-12-19 16:45:39 +0800

    v cam

--- src/components/VTimeline/index.tsx
+++ src/components/VTimeline/index.tsx
@@ -1,12 +1,13 @@
 import VTimelineToolbar from "@/components/VTimeline/Toolbar"
 import VTimelineTrackLeftPanel from "@/components/VTimeline/TrackLeftPanel"
 import VTimelineTracks from "@/components/VTimeline/Tracks"
+import { Suspense } from "vue"
 
 export default defineComponent({
-  async setup() {
+  setup() {
     const timeline = useTimelineStore()
 
-    await timeline.initBlocks()
+    timeline.initBlocks()
     return () => (
       <div class="relative h-[--timeline-height] w-full overflow-scroll border-x-[1px] border-b-[1px] border-zinc-100 bg-zinc-50 p-0">
         {/* VTimeToolbar */}
